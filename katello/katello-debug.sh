9b8f888204074e481799775c8f9f0017ce927cd7
Mike McCune <mmccune@redhat.com>
2016-01-25 09:58:04 -0800

    Fixes #13363 - Adding additional qpid debug statements to foreman-debug

--- katello/katello-debug.sh
+++ katello/katello-debug.sh
@@ -13,6 +13,22 @@ then
   exit 1
 fi
 
+# foreman-debug will truncate any file beyond 5k lines
+# for larger files we need to copy the entire file.
+copy_files() {
+  for FILE in $*; do
+    echo "Copying entire file: $FILE"
+    if [ \( -f "$FILE" -o -h "$FILE" \) -a \( -r "$FILE" -a -s "$FILE" \) ]; then
+      printv " - $FILE"
+      SUBDIR=$(dirname $FILE)
+      [ ! -d "$DIR$SUBDIR" ] && mkdir -p "$DIR$SUBDIR"
+      cp "$FILE" "$DIR$FILE"
+    fi
+  done
+}
+
+
+
 # Installer
 add_files /var/log/{katello,capsule}-installer/*
 add_files /etc/{katello,capsule}-installer/*
@@ -53,7 +69,7 @@ if [ $NOGENERIC -eq 0 ]; then
   add_files /etc/qpidd.conf
 fi
 add_cmd "qpid-stat --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671 -q" "qpid_stat_queues"
-add_cmd "qpid-stat --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671 -u" "qpid_stat_subscriptions" 
+add_cmd "qpid-stat --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671 -u" "qpid_stat_subscriptions"
 
 # Gofer
 add_files /etc/gofer
@@ -63,7 +79,7 @@ add_files /var/log/gofer
 if hash foreman-rake  2>/dev/null; then
   echo "Exporting tasks, this may take a few minutes."
   tasks_filename=`foreman-rake foreman_tasks:export_tasks 2> /tmp/tasks_export.log | tail -n 1 | awk '{print $2}'`
-  add_files $tasks_filename
+  copy_files $tasks_filename
   add_files /tmp/tasks_export.log
 fi
 
@@ -80,6 +96,22 @@ fi
 # Squid
 add_files /etc/squid/squid.conf
 add_files /var/log/squid/*
+
+# Qpid Debug
+add_cmd "qpid-stat -q --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671" "qpid-stat-q"
+add_cmd "qpid-stat -u --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671" "qpid-stat-u"
+add_cmd "qpid-stat -c --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt -b amqps://localhost:5671" "qpid-stat-c"
+add_cmd "qpid-stat --ssl-certificate=/etc/pki/katello/qpid_client_striped.crt  -b amqps://localhost:5671 -q resource_manager" "qpid-stat-resource_manager"
+add_cmd "ps -awfux" "ps-awfux"
+add_cmd "ps -efLm" "ps-elfm"
+add_cmd "rpm -qa | grep qpid" "qpid-rpm-qa"
+
+add_cmd "mongo pulp_database --eval \"db.reserved_resources.find().pretty().shellPrint()\"" "mongo-reserved_resources"
+add_cmd "mongo pulp_database --eval \"db.task_status.find().pretty().shellPrint()\"" "mongo-task_status"
+
+echo "db.task_status.find({state:{\$ne: \"finished\"}}).pretty().shellPrint()" > /tmp/pulp_running_tasks.js
+
+add_cmd "mongo pulp_database /tmp/pulp_running_tasks.js" "pulp-running_tasks"
 
 # Legend:
 # * - already collected by sosreport tool (skip when -g option was provided)
