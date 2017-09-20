4f33ccd9434e9525dc90918c9eaee5846cb6a7a1
Evgeni Golov <evgeni@golov.de>
2017-09-20 12:02:59 +0200

    Fixes #21029 - define fail_with_message before run_cmd
    run_cmd uses fail_with_message, so it has to be defined before

--- katello/helper.rb
+++ katello/helper.rb
@@ -16,6 +16,12 @@ module KatelloUtilities
       system("rpm -q foreman")
     end
 
+    def fail_with_message(message, opt_parser=nil)
+      STDOUT.puts message
+      puts opt_parser if opt_parser
+      exit(false)
+    end
+
     def run_cmd(command, exit_codes=[0], message=nil)
       result = `#{command}`
       unless exit_codes.include?($?.exitstatus)
@@ -33,12 +39,6 @@ module KatelloUtilities
 
     def timestamp
       DateTime.now.strftime('%Y%m%d%H%M%S')
-    end
-
-    def fail_with_message(message, opt_parser=nil)
-      STDOUT.puts message
-      puts opt_parser if opt_parser
-      exit(false)
     end
   end
 end
